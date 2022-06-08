d7fb70820af6ae1e8261d59d9feec5e52ffd0789
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-06-07 21:43:01 -0300

    a

--- Lista de Tarefas/src/JS/main.js
+++ Lista de Tarefas/src/JS/main.js
@@ -1,43 +1,21 @@
-function criaHoraDosSegundos (segundos) {
-    let data = new Date(segundos * 1000);
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const content = document.querySelector('.items');
 
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone:'GMT'
-    });
+button.addEventListener('click', () => {
+    if (!input.value) return;
+    createTask(input.value);
+
+    
+});
+
+function createTask (textcontent) {
+    const li = createLi();
+    li.innerHTML = textcontent;
+    content.appendChild(li);
 }
 
-const relogio = document.querySelector('h2');
-const iniciar = document.querySelector('button:nth-child(1)');
-const pausar = document.querySelector('button:nth-child(2)');
-const zerar = document.querySelector('button:nth-child(3)');
-let segundos = 0;
-let timer;
-
-
-
-iniciar.addEventListener('click', () => {
-    relogio.classList.remove('pausado');
-    clearInterval(timer);
-    iniciaRelogio();
-});
-pausar.addEventListener('click', () => {
-    relogio.classList.add('pausado');
-    clearInterval(timer);
-});
-zerar.addEventListener('click',() => {
-    clearInterval(timer);
-    relogio.innerHTML = '00:00:00';
-    relogio.classList.remove('pausado');
-    segundos = 0;
-} );
-
-
-function iniciaRelogio () {
-    timer = setInterval(function() {
-        segundos++;
-        relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000);
+function createLi () {
+    const li = document.createElement('li');
+    return li;
 }
-
-
