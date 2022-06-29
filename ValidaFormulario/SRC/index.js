8bd3b39d5e4b1299cfa38a35b9d04337c86b9365
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-06-29 10:05:12 -0300

    .

---
+++ ValidaFormulario/SRC/index.js
@@ -0,0 +1,19 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario');
+        this.eventos();
+    }
+
+    eventos () {
+        this.formulario.addEventListener('submit', (e) => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        
+    }
+}
+
+const valida = new ValidaFormulario();
