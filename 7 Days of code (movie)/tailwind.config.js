c5ebece54a0d13509ed9b32bbf56067502da817f
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-08-20 02:42:27 -0300

    Inicio

---
+++ 7 Days of code (movie)/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#17162E'
+      },
+      spacing: {
+        '100': '100%'
+      }
+
+    },
+  },
+  plugins: [],
+}
